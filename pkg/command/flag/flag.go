e4fa01b2931d0747e17f2079ea6f4d15344d4315
Michał Matczuk <michal@scylladb.com>
2021-11-09 15:37:23 +0100

    command/flag: add cluster

--- pkg/command/flag/flag.go
+++ pkg/command/flag/flag.go
@@ -2,44 +2,52 @@
 
 package flag
 
-import flag "github.com/spf13/pflag"
+import (
+	"os"
+
+	flag "github.com/spf13/pflag"
+)
 
 type Wrapper struct {
-	*flag.FlagSet
+	fs *flag.FlagSet
 }
 
 func Wrap(fs *flag.FlagSet) Wrapper {
-	return Wrapper{FlagSet: fs}
+	return Wrapper{fs: fs}
 }
 
 //
-// Common task flags
+// Task schedule flags
 //
 
 func (w Wrapper) Interval(p *Duration) {
-	w.VarP(p, "interval", "i", usage["interval"])
+	w.fs.VarP(p, "interval", "i", usage["interval"])
 }
 
 func (w Wrapper) StartDate(p *Time) {
-	w.VarP(p, "start-date", "s", usage["start-date"])
+	w.fs.VarP(p, "start-date", "s", usage["start-date"])
 }
 
-func (w Wrapper) NumRetries(p *int64, def int64) {
-	w.Int64VarP(p, "num-retries", "r", def, usage["num-retries"])
+func (w Wrapper) NumRetries(p *int, def int) {
+	w.fs.IntVarP(p, "num-retries", "r", def, usage["num-retries"])
 }
 
 //
 // Common flags
 //
 
+func (w Wrapper) Cluster(p *string) {
+	w.fs.StringVarP(p, "cluster", "c", os.Getenv("SCYLLA_MANAGER_CLUSTER"), usage["cluster"])
+}
+
 func (w Wrapper) Datacenter(p *[]string) {
-	w.StringSliceVar(p, "dc", nil, usage["dc"])
+	w.fs.StringSliceVar(p, "dc", nil, usage["dc"])
 }
 
 func (w Wrapper) Keyspace(p *[]string) {
-	w.StringSliceVarP(p, "keyspace", "K", nil, usage["keyspace"])
+	w.fs.StringSliceVarP(p, "keyspace", "K", nil, usage["keyspace"])
 }
 
 func (w Wrapper) Location(p *[]string) {
-	w.StringSliceVarP(p, "location", "L", nil, usage["location"])
+	w.fs.StringSliceVarP(p, "location", "L", nil, usage["location"])
 }
